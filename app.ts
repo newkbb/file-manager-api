7375731512a945859c918090bfd7511acc3b67e0
newkbb <18149335503@163.com>
2022-07-20 17:21:52 +0800

    编写ts文件，添加一些query

--- app.ts
+++ app.ts
@@ -1,6 +1,62 @@
 // eslint-disable-next-line
 require("dotenv").config()
 import express from "express"
+import { graphqlHTTP } from "express-graphql"
+import { makeExecutableSchema } from "@graphql-tools/schema"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+const typeDefs = `
+  type File {
+    id:String!
+    name:String!
+    directoryID:String!
+    createdAt:String!
+    updatedAt:String!
+    versions:[FileVersion]
+  }
+
+  type FileVersion {
+    id:String!
+    name:String!  
+    mimeType:String!
+    size:Int!
+    fileID:String!      
+    createdAt:String!
+    updatedAt:String!  
+  }
+
+  type Directory{
+    id:String!
+    name:String!
+    parentID:String?
+    createdAt:String!
+    updatedAt:String!   
+    files[File]
+    directories Directory[]     
+  }
+
+  type Query {
+    getAllFiles: [File]!
+    getAllFileVersions: [FileVersion]!
+    getAllDirectories: [Directory]!
+  }
+`
+
+const resolvers = {
+  Query: {
+    getAllFiles: () => {
+      return prisma.file.findMany()
+    },
+    getAllFileVersions: () => {
+      return prisma.fileVersion.findMany()
+    },
+    getAllDirectories: () => {
+      return prisma.directory.findMany()
+    }
+  }
+}
 
 const app = express()
 const port = 3000
